67273e7c265bed000e1dc8997993216936351aa0
Alexander Bigerl <bigerl@mail.upb.de>
2020-05-26 16:59:50 +0200

    Node tests done

--- tests/node_based/TestNode.hpp
+++ tests/node_based/TestNode.hpp
@@ -1,112 +1,166 @@
 #ifndef HYPERTRIE_TESTNODE_HPP
 #define HYPERTRIE_TESTNODE_HPP
 
+#include <boost/type_index.hpp>
+#include <fmt/format.h>
+#include <enumerate.hpp>
+
 #include <Dice/hypertrie/internal/node_based/Node.hpp>
 
 #include "../utils/AssetGenerator.hpp"
 
 namespace hypertrie::tests::node_based::node {
 
-	using namespace hypertrie::internal::node_based;
+    template<typename T>
+    auto name_of_type() {
+        return std::string{boost::typeindex::type_id_with_cvr<T>().pretty_name()};
+    }
 
-	template<size_t depth, typename key_part_type>
-	using Key = hypertrie::internal::RawKey<depth, key_part_type>;
+    using namespace hypertrie::internal::node_based;
 
-	template<HypertrieInternalTrait tr, pos_type depth>
-	void createCompressedNode() {
-		using key_part_type = typename tr::key_part_type;
-		using value_type = typename tr::value_type;
-		using Key = Key<depth, key_part_type>;
+    template<size_t depth, typename key_part_type>
+    using Key = hypertrie::internal::RawKey<depth, key_part_type>;
 
-		utils::RawGenerator<depth, key_part_type, value_type> gen{};
+    template<HypertrieInternalTrait tr, pos_type depth>
+    void createCompressedNode() {
+        using key_part_type = typename tr::key_part_type;
+        using value_type = typename tr::value_type;
+        using Key = Key<depth, key_part_type>;
 
-		Key key = gen.key();
+        SECTION(fmt::format("depth = {}, key_part_type = {}, value_type = {}",
+                            depth, name_of_type<key_part_type>(), name_of_type<value_type>())) {
+            utils::RawGenerator<depth, key_part_type, value_type> gen{};
 
-		// unused for bool
-		value_type value = gen.value();
+            Key key = gen.key();
 
-		Node<depth, true, tr> node = [&]() {
-			if constexpr (tr::is_bool_valued) return Node<depth, true, tr>{key};
-			else return Node<depth, true, tr>{key, value};
-		}();
+            // unused for bool
+            value_type value = gen.value();
 
-		REQUIRE(node.key() == key);
-		if constexpr (not std::is_same_v<value_type, bool>)
-			REQUIRE(node.value() == value);
-	}
+            CompressedNode<depth, tr> node = [&]() {
+                if constexpr (tr::is_bool_valued) return CompressedNode<depth, tr>{key};
+                else return CompressedNode<depth, tr>{key, value};
+            }();
 
-	TEST_CASE("create compressed node", "[Node]") {
-		for ([[maybe_unused]] auto _ : iter::range(50)) {
-			createCompressedNode<default_bool_Hypertrie_internal_t, 1>();
-			createCompressedNode<default_long_Hypertrie_internal_t, 1>();
-			createCompressedNode<default_double_Hypertrie_internal_t, 1>();
-			createCompressedNode<default_bool_Hypertrie_internal_t, 2>();
-			createCompressedNode<default_long_Hypertrie_internal_t, 2>();
-			createCompressedNode<default_double_Hypertrie_internal_t, 2>();
-			createCompressedNode<default_bool_Hypertrie_internal_t, 3>();
-			createCompressedNode<default_long_Hypertrie_internal_t, 3>();
-			createCompressedNode<default_double_Hypertrie_internal_t, 3>();
-		}
-	}
+            REQUIRE(node.key() == key);
+            if constexpr (not std::is_same_v<value_type, bool>)
+                REQUIRE(node.value() == value);
+        }
+    }
 
-	template<HypertrieInternalTrait tr, pos_type depth>
-	void createEmptyUncompressed() {
+    TEST_CASE("create compressed node", "[Node]") {
+        createCompressedNode<default_bool_Hypertrie_internal_t, 1>();
+        createCompressedNode<default_long_Hypertrie_internal_t, 1>();
+        createCompressedNode<default_double_Hypertrie_internal_t, 1>();
+        createCompressedNode<default_bool_Hypertrie_internal_t, 2>();
+        createCompressedNode<default_long_Hypertrie_internal_t, 2>();
+        createCompressedNode<default_double_Hypertrie_internal_t, 2>();
+        createCompressedNode<default_bool_Hypertrie_internal_t, 3>();
+        createCompressedNode<default_long_Hypertrie_internal_t, 3>();
+        createCompressedNode<default_double_Hypertrie_internal_t, 3>();
+    }
 
-		Node<depth, false, tr> node{};
+    template<HypertrieInternalTrait tr, pos_type depth>
+    void createEmptyUncompressed() {
+        using key_part_type = typename tr::key_part_type;
+        using value_type = typename tr::value_type;
+        SECTION(fmt::format("depth = {}, key_part_type = {}, value_type = {}",
+                            depth, name_of_type<key_part_type>(), name_of_type<value_type>())) {
 
-		for (size_t pos : iter::range(depth))
-			REQUIRE(node.edges(pos).size() == 0);
-		REQUIRE(node.size() == 0);
-	}
+            UncompressedNode<depth, tr> node{};
 
-	TEST_CASE("create empty uncompressed node", "[Node]") {
-		createEmptyUncompressed<default_bool_Hypertrie_internal_t, 1>();
-		createEmptyUncompressed<default_long_Hypertrie_internal_t, 1>();
-		createEmptyUncompressed<default_double_Hypertrie_internal_t, 1>();
-		createEmptyUncompressed<default_bool_Hypertrie_internal_t, 2>();
-		createEmptyUncompressed<default_long_Hypertrie_internal_t, 2>();
-		createEmptyUncompressed<default_double_Hypertrie_internal_t, 2>();
-		createEmptyUncompressed<default_bool_Hypertrie_internal_t, 3>();
-		createEmptyUncompressed<default_long_Hypertrie_internal_t, 3>();
-		createEmptyUncompressed<default_double_Hypertrie_internal_t, 3>();
-	}
+            for (size_t pos : iter::range(depth))
+                REQUIRE(node.edges(pos).size() == 0);
+            REQUIRE(node.size() == 0);
+        }
+    }
 
-	template<HypertrieInternalTrait tr, pos_type depth>
-	void insertSingleEntryIntoEmptyUncompressed() {
-		using key_part_type = typename tr::key_part_type;
-		using value_type = typename tr::value_type;
-		using Key = Key<depth, key_part_type>;
+    TEST_CASE("create empty uncompressed node", "[Node]") {
+        createEmptyUncompressed<default_bool_Hypertrie_internal_t, 1>();
+        createEmptyUncompressed<default_long_Hypertrie_internal_t, 1>();
+        createEmptyUncompressed<default_double_Hypertrie_internal_t, 1>();
+        createEmptyUncompressed<default_bool_Hypertrie_internal_t, 2>();
+        createEmptyUncompressed<default_long_Hypertrie_internal_t, 2>();
+        createEmptyUncompressed<default_double_Hypertrie_internal_t, 2>();
+        createEmptyUncompressed<default_bool_Hypertrie_internal_t, 3>();
+        createEmptyUncompressed<default_long_Hypertrie_internal_t, 3>();
+        createEmptyUncompressed<default_double_Hypertrie_internal_t, 3>();
+    }
 
-		utils::RawGenerator<depth, key_part_type, value_type> gen{};
+    template<HypertrieInternalTrait tr, pos_type depth>
+    void insertSingleEntryIntoEmptyUncompressed() {
+        using key_part_type = typename tr::key_part_type;
+        using value_type = typename tr::value_type;
+        using Key = Key<depth, key_part_type>;
+        SECTION(fmt::format("depth = {}, key_part_type = {}, value_type = {}",
+                            depth, name_of_type<key_part_type>(), name_of_type<value_type>())) {
 
-		UncompressedNode<depth, tr> node{};
+            utils::RawGenerator<depth, key_part_type, value_type> gen{};
 
-		auto[key, value] = gen.entry();
+            UncompressedNode<depth, tr> node{};
 
-		node.insertEntry(key, value);
+            auto[key, value] = gen.entry();
 
-		if constexpr(depth == 1) {
-			REQUIRE(node.child(0, key[0]) == value);
-		} else {
-			for (auto pos : iter::range(depth)) {
-				auto expected_hash = TaggedNodeHash::getCompressedNodeHash(tr::subkey(key, pos), value);
-				REQUIRE(node.edges(pos)[key[pos]] == expected_hash);
-			}
-		}
-	}
+            // insert a single entry
+            {
+                node.insertEntry(key, value);
+
+                if constexpr(depth == 1) {
+                    REQUIRE(node.child(0, key[0]) == value);
+                } else {
+                    for (auto pos : iter::range(depth)) {
+                        auto expected_hash = TaggedNodeHash::getCompressedNodeHash(tr::subkey(key, pos), value);
+                        REQUIRE(node.edges(pos)[key[pos]] == expected_hash);
+                    }
+                }
+            }
+            // insert another value with a common position
+            if constexpr (depth > 1) {
+                for (auto unchanged_pos : iter::range(depth)) {
+                    auto node2 = node;
+                    auto second_value = gen.value();
+                    auto second_key = key;
+                    // increase all but one entries of the second key by 1
+                    for (auto[pos, key_part] : iter::enumerate(second_key))
+                        if (pos != unchanged_pos)
+                            key_part += key_part_type(1);
+
+                    // insert the second entry
+                    node2.insertEntry(second_key, second_value);
+
+                    for (auto pos : iter::range(depth)) {
+                        if (pos == unchanged_pos) { // there is a sub node with two entries for unchanged_pos
+                            auto expected_hash = TaggedNodeHash::getCompressedNodeHash(tr::subkey(key, pos), value)
+                                    .addEntry(tr::subkey(second_key, pos), second_value);
+                            REQUIRE(node2.edges(pos)[key[pos]] == expected_hash);
+                        } else { // there are single entries for all other positions
+                            { // entry for key/value
+                                auto expected_hash = TaggedNodeHash::getCompressedNodeHash(tr::subkey(key, pos), value);
+                                REQUIRE(node2.edges(pos)[key[pos]] == expected_hash);
+                            }
+                            { // entry for second_key/value
+                                auto expected_hash = TaggedNodeHash::getCompressedNodeHash(tr::subkey(second_key, pos),
+                                                                                           second_value);
+                                REQUIRE(node2.edges(pos)[second_key[pos]] == expected_hash);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
-	TEST_CASE("insert single entry into uncompressed node", "[Node]") {
-		insertSingleEntryIntoEmptyUncompressed<default_bool_Hypertrie_internal_t, 1>();
-		insertSingleEntryIntoEmptyUncompressed<default_long_Hypertrie_internal_t, 1>();
-		insertSingleEntryIntoEmptyUncompressed<default_double_Hypertrie_internal_t, 1>();
-		insertSingleEntryIntoEmptyUncompressed<default_bool_Hypertrie_internal_t, 2>();
-		insertSingleEntryIntoEmptyUncompressed<default_long_Hypertrie_internal_t, 2>();
-		insertSingleEntryIntoEmptyUncompressed<default_double_Hypertrie_internal_t, 2>();
-		insertSingleEntryIntoEmptyUncompressed<default_bool_Hypertrie_internal_t, 3>();
-		insertSingleEntryIntoEmptyUncompressed<default_long_Hypertrie_internal_t, 3>();
-		insertSingleEntryIntoEmptyUncompressed<default_double_Hypertrie_internal_t, 3>();
-	}
+    TEST_CASE("insert single entry into uncompressed node", "[Node]") {
+        insertSingleEntryIntoEmptyUncompressed<default_bool_Hypertrie_internal_t, 1>();
+        insertSingleEntryIntoEmptyUncompressed<default_long_Hypertrie_internal_t, 1>();
+        insertSingleEntryIntoEmptyUncompressed<default_double_Hypertrie_internal_t, 1>();
+        insertSingleEntryIntoEmptyUncompressed<default_bool_Hypertrie_internal_t, 2>();
+        insertSingleEntryIntoEmptyUncompressed<default_long_Hypertrie_internal_t, 2>();
+        insertSingleEntryIntoEmptyUncompressed<default_double_Hypertrie_internal_t, 2>();
+        insertSingleEntryIntoEmptyUncompressed<default_bool_Hypertrie_internal_t, 3>();
+        insertSingleEntryIntoEmptyUncompressed<default_long_Hypertrie_internal_t, 3>();
+        insertSingleEntryIntoEmptyUncompressed<default_double_Hypertrie_internal_t, 3>();
+    }
 
 };
 

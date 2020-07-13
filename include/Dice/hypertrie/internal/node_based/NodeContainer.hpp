68ac5215088709f039b69a7588ec738f15068326
Alexander Bigerl <bigerl@mail.upb.de>
2020-07-13 12:01:49 +0200

    introduced NodePtr type in NodeContainer

--- include/Dice/hypertrie/internal/node_based/NodeContainer.hpp
+++ include/Dice/hypertrie/internal/node_based/NodeContainer.hpp
@@ -17,62 +17,92 @@ namespace hypertrie::internal::node_based {
 			 HypertrieInternalTrait tri_t = Hypertrie_internal_t<>>
 	using UncompressedNodeContainer = SpecificNodeContainer<depth, NodeCompression::uncompressed, tri_t>;
 
+	template<size_t depth,
+			 HypertrieInternalTrait tri_t = Hypertrie_internal_t<>>
+	typedef union NodePtr {
+		NodePtr() noexcept : raw(nullptr){}
+		NodePtr(const NodePtr &node_ptr) noexcept : raw(node_ptr.raw){}
+		NodePtr(NodePtr &&node_ptr) noexcept : raw(node_ptr.raw){}
+		NodePtr(void *raw) noexcept : raw(raw){}
+		NodePtr(UncompressedNode<depth, tri_t> *uncompressed) noexcept : uncompressed(uncompressed){}
+		NodePtr(CompressedNode<depth, tri_t> *compressed) noexcept : compressed(compressed){}
+		void *raw;
+		UncompressedNode<depth, tri_t> *uncompressed;
+		CompressedNode<depth, tri_t> *compressed;
+
+		operator CompressedNode<depth, tri_t> *() const noexcept { return this->compressed; }
+		operator UncompressedNode<depth, tri_t> *() const noexcept { return this->uncompressed; }
+
+		NodePtr &operator=(const NodePtr &node_ptr) noexcept {
+			raw = node_ptr.raw;
+			return *this;
+		}
+
+		NodePtr &operator=(NodePtr &&node_ptr) noexcept {
+			raw = node_ptr.raw;
+			return *this;
+		}
+	};
+
 	template<size_t depth, HypertrieInternalTrait tri_t = Hypertrie_internal_t<>>
 	struct NodeContainer {
-		TaggedNodeHash thash_{};
-
 	protected:
-		void *node_ = nullptr;
+		TaggedNodeHash hash_{};
+		NodePtr<depth, tri_t> node_ptr_{};
 
 	public:
-		NodeContainer() {}
+		NodeContainer() noexcept {}
 
-		NodeContainer(const TaggedNodeHash &thash, void *node) : thash_(thash), node_(node) {}
-
-		template<NodeCompression compressed>
-		NodeContainer(const SpecificNodeContainer<depth, compressed, tri_t> &other) : thash_{other.thash_}, node_{other.node_} {}
-		template<NodeCompression compressed>
-		NodeContainer(SpecificNodeContainer<depth, compressed, tri_t> &&other) : thash_{other.thash_}, node_{other.node_} {}
-
-		[[nodiscard]] auto compressed_node() {
-			return static_cast<CompressedNode<depth, tri_t> *>(node_);
-		}
-
-		[[nodiscard]] auto uncompressed_node() {
-			return static_cast<UncompressedNode<depth, tri_t> *>(node_);
-		}
+		NodeContainer(const TaggedNodeHash &thash, NodePtr<depth, tri_t> node) noexcept : hash_(thash), node_ptr_(node) {}
 
 		template<NodeCompression compressed>
-		[[nodiscard]] auto specific() {
-			return SpecificNodeContainer<depth, compressed, tri_t>(thash_, node_);
+		NodeContainer(const SpecificNodeContainer<depth, compressed, tri_t> &other) noexcept : hash_{other.hash_}, node_ptr_{other.node_ptr_} {}
+		template<NodeCompression compressed>
+		NodeContainer(SpecificNodeContainer<depth, compressed, tri_t> &&other) noexcept : hash_{other.hash_}, node_ptr_{other.node_ptr_} {}
+
+		[[nodiscard]] auto * &compressed_node() noexcept { return node_ptr_.compressed; }
+		[[nodiscard]] auto * compressed_node() const noexcept  { return node_ptr_.compressed; }
+
+		[[nodiscard]] auto * &uncompressed_node() noexcept { return node_ptr_.uncompressed; }
+		[[nodiscard]] auto * uncompressed_node() const noexcept  { return node_ptr_.uncompressed; }
+
+		[[nodiscard]] auto * &node() noexcept { return node_ptr_.raw; }
+		[[nodiscard]] auto * node() const noexcept  { return node_ptr_.raw; }
+
+		template<NodeCompression compression>
+		[[nodiscard]] auto specific() const noexcept { return SpecificNodeContainer<depth, compression, tri_t>{hash_, node_ptr_}; }
+
+		[[nodiscard]] auto compressed() const noexcept { return specific<NodeCompression::compressed>(); }
+
+		[[nodiscard]] auto uncompressed() const noexcept { return specific<NodeCompression::uncompressed>(); }
+
+		[[nodiscard]] bool isCompressed() const noexcept { return hash_.isCompressed(); }
+
+		[[nodiscard]] bool isUncompressed() const noexcept { return hash_.isUncompressed(); }
+
+		[[nodiscard]] bool empty() const noexcept { return hash_.empty(); }
+
+		[[nodiscard]] bool null() const noexcept { return node_ptr_.raw == nullptr; }
+
+		[[nodiscard]] TaggedNodeHash hash() const noexcept { return this->hash_; }
+
+		[[nodiscard]] TaggedNodeHash &hash() noexcept { return this->hash_; }
+
+		size_t &ref_count() {
+			if (isCompressed())
+				return compressed_node()->ref_count();
+			else
+				return uncompressed_node()->ref_count();
 		}
 
-		[[nodiscard]] auto compressed() {
-			return specific<NodeCompression::compressed>();
-		}
-
-		[[nodiscard]] auto uncompressed() {
-			return specific<NodeCompression::uncompressed>();
-		}
-
-		[[nodiscard]] bool isCompressed() const { return thash_.isCompressed(); }
-
-		[[nodiscard]] bool isUncompressed() const { return thash_.isUncompressed(); }
-
-		[[nodiscard]] bool empty() const { return thash_ == TaggedNodeHash{}; }
-
-		[[nodiscard]] bool null() const { return node_ == nullptr; }
-
-		operator TaggedNodeHash() const {
-			return this->thash_;
-		}
+		operator TaggedNodeHash() const noexcept  { return this->hash_; }
 
 		friend struct SpecificNodeContainer<depth, NodeCompression::compressed, tri_t>;
 		friend struct SpecificNodeContainer<depth, NodeCompression::uncompressed, tri_t>;
 	};
 
 	template<NodeCompression compression, HypertrieInternalTrait tri>
-	auto getValue(SpecificNodeContainer<1, compression, tri> &nodec, typename tri::key_part_type key_part)
+	auto getValue(SpecificNodeContainer<1, compression, tri> &nodec, typename tri::key_part_type key_part) noexcept
 			-> typename tri::value_type {
 		if constexpr (compression == NodeCompression::compressed) {
 			if (key_part == nodec.node()->key()[0]) {
@@ -89,25 +119,29 @@ namespace hypertrie::internal::node_based {
 	template<size_t depth, HypertrieInternalTrait tri_t>
 	struct SpecificNodeContainer<depth, NodeCompression::compressed, tri_t> : public NodeContainer<depth, tri_t> {
 
-		SpecificNodeContainer() : NodeContainer<depth, tri_t>{} {}
+		SpecificNodeContainer() noexcept : NodeContainer<depth, tri_t>{} {}
 
-		SpecificNodeContainer(const TaggedNodeHash &thash, void *node) : NodeContainer<depth, tri_t>(thash, node) {}
+		SpecificNodeContainer(const SpecificNodeContainer & nodec) noexcept : NodeContainer<depth, tri_t>{nodec} {}
+
+		SpecificNodeContainer(SpecificNodeContainer && nodec) noexcept : NodeContainer<depth, tri_t>{nodec} {}
+
+		SpecificNodeContainer(const TaggedNodeHash &thash, NodePtr<depth, tri_t> node) noexcept : NodeContainer<depth, tri_t>{thash, node} {}
+
+		SpecificNodeContainer& operator=(const SpecificNodeContainer &nodec) =default;
+
+		SpecificNodeContainer& operator=(SpecificNodeContainer &&nodec) =default;
 
 
-		[[nodiscard]] constexpr auto node() {
-			return static_cast<CompressedNode<depth, tri_t> *>(this->node_);
-		}
+		[[nodiscard]] constexpr auto node() noexcept { return this->node_ptr_.compressed; }
 
-		[[nodiscard]] constexpr auto node() const{
-			return static_cast<CompressedNode<depth, tri_t> *>(this->node_);
-		}
+		[[nodiscard]] constexpr auto node() const noexcept  { return this->node_ptr_.compressed; }
 
-		[[nodiscard]] bool isCompressed() const { return true; }
+		[[nodiscard]] bool isCompressed() const noexcept  { return true; }
 
-		[[nodiscard]] bool isUncompressed() const { return false; }
+		[[nodiscard]] bool isUncompressed() const noexcept  { return false; }
 
-		operator NodeContainer<depth, tri_t>() const { return {this->thash_, this->node_}; }
-		operator NodeContainer<depth, tri_t> &&() const { return {this->thash_, this->node_}; }
+		operator NodeContainer<depth, tri_t>() const noexcept { return {this->hash_, this->node_ptr_}; }
+		operator NodeContainer<depth, tri_t> &&() const noexcept { return {this->hash_, this->node_ptr_}; }
 	};
 
 
@@ -115,21 +149,25 @@ namespace hypertrie::internal::node_based {
 			 HypertrieInternalTrait tri_t>
 	struct SpecificNodeContainer<depth, NodeCompression::uncompressed, tri_t> : public NodeContainer<depth, tri_t> {
 
-		SpecificNodeContainer() : NodeContainer<depth, tri_t>{} {}
+		SpecificNodeContainer() noexcept : NodeContainer<depth, tri_t>{} {}
 
-		SpecificNodeContainer(const TaggedNodeHash &thash, void *node) : NodeContainer<depth, tri_t>(thash, node) {}
+		SpecificNodeContainer(const SpecificNodeContainer & nodec) noexcept : NodeContainer<depth, tri_t>{nodec} {}
 
-		[[nodiscard]] constexpr auto node() {
-			return static_cast<UncompressedNode<depth, tri_t> *>(this->node_);
-		}
+		SpecificNodeContainer(SpecificNodeContainer && nodec) noexcept : NodeContainer<depth, tri_t>{nodec} {}
 
-		[[nodiscard]] constexpr auto node() const {
-			return static_cast<UncompressedNode<depth, tri_t> *>(this->node_);
-		}
+		SpecificNodeContainer(const TaggedNodeHash &thash, NodePtr<depth, tri_t> node) noexcept : NodeContainer<depth, tri_t>{thash, node} {}
 
-		[[nodiscard]] bool isCompressed() const { return false; }
+		SpecificNodeContainer& operator=(const SpecificNodeContainer &nodec) =default;
 
-		[[nodiscard]] bool isUncompressed() const { return true; }
+		SpecificNodeContainer& operator=(SpecificNodeContainer &&nodec) =default;
+
+		[[nodiscard]] constexpr auto node() noexcept { return this->node_ptr_.uncompressed; }
+
+		[[nodiscard]] constexpr auto node() const noexcept { return this->node_ptr_.uncompressed; }
+
+		[[nodiscard]] bool isCompressed() const noexcept { return false; }
+
+		[[nodiscard]] bool isUncompressed() const noexcept { return true; }
 
 		inline auto getChildHashOrValue(size_t pos, typename tri_t::key_part_type key_part)
 				-> std::conditional_t<(depth > 1), TaggedNodeHash, typename tri_t::value_type> {
@@ -137,8 +175,8 @@ namespace hypertrie::internal::node_based {
 			return this->node()->child(pos, key_part);
 		}
 
-		operator NodeContainer<depth, tri_t>() const { return {this->thash_, this->node_}; }
-		operator NodeContainer<depth, tri_t> &&() const { return {this->thash_, this->node_}; }
+		operator NodeContainer<depth, tri_t>() const noexcept { return {this->hash_, this->node_ptr_}; }
+		operator NodeContainer<depth, tri_t> &&() const noexcept { return {this->hash_, this->node_ptr_}; }
 	};// namespace hypertrie::internal::node_based
 
 

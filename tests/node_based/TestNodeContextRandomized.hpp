6c9687a7ab5ffa7e79d8f79d01252968a63698d3
Alexander Bigerl <bigerl@mail.upb.de>
2020-07-23 17:09:31 +0200

    added double bulk for unused_lsb

--- tests/node_based/TestNodeContextRandomized.hpp
+++ tests/node_based/TestNodeContextRandomized.hpp
@@ -117,6 +117,68 @@ namespace hypertrie::tests::node_based::node_context::randomized {
 			}
 	}
 
+	TEST_CASE("Test Randomized double bulk long -> bool, unused_lsb", "[NodeContext]") {
+		using tr = Hypertrie_internal_t<Hypertrie_t<unsigned long,
+				bool,
+				hypertrie::internal::container::std_map,
+				hypertrie::internal::container::std_set,
+				true>>;
+		constexpr pos_type depth = 3;
+
+		using key_part_type = typename tr::key_part_type;
+		using value_type = typename tr::value_type;
+		using Key = typename tr::template RawKey<depth>;
+
+		static utils::RawGenerator<depth, key_part_type, value_type, 0, 10> gen{};
+
+		NodeContext<depth, tr> context{};
+		// create emtpy primary node
+		UncompressedNodeContainer<depth, tr> nc = context.template newPrimaryNode<depth>();
+		auto tt = TestTensor<depth, tr>::getPrimary();
+
+
+		for (size_t count : iter::range(3,50))
+			SECTION("insert {} key "_format(count)) {
+				for (const auto i : iter::range(10)) {
+					SECTION("{}"_format(i)) {
+						// generate entries
+						auto temp_keys = gen.keys(2*count);
+						std::vector<Key> temp_keys_vec{temp_keys.begin(), temp_keys.end()};
+						for (auto &key : temp_keys_vec){
+							key[0] <<= 1;
+							key[1] <<= 1;
+							key[2] <<= 1;
+						}
+						// generate entries
+						auto middle_it = temp_keys_vec.begin();
+						std::advance(middle_it,count);
+						std::vector<std::vector<Key>> keyss{
+								{temp_keys_vec.begin(), middle_it},
+								{middle_it, temp_keys_vec.end()}};
+
+						for (std::vector<Key> &keys: keyss){
+							// print entries
+							std::string print_entries{};
+							for (auto &key : keys)
+								print_entries += "{} → true\n"_format(key);
+							WARN(print_entries);
+
+							// insert entries into test tensor
+							for (auto &key : keys) {
+								tt.set(key, true);
+							}
+
+							// bulk insert keys
+							context.template bulk_insert<depth>(nc, keys);
+							WARN("\n\n\nresulting hypertrie \n{}\n\n"_format((std::string) context.storage));
+							// check if they were inserted correctly
+							tt.checkContext(context);
+						}
+					}
+				}
+			}
+	}
+
 	TEST_CASE("Test Randomized long -> bool", "[NodeContext]") {
 		using tr = default_bool_Hypertrie_internal_t;
 		constexpr pos_type depth = 3;

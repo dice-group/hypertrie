36a1792b74a74d5bf724a66e1a614d1af12c0419
Alexander Bigerl <bigerl@mail.upb.de>
2020-08-01 19:55:46 +0200

    implemented basic set and get methods in node-based hypertrie

--- include/Dice/hypertrie/internal/node_based/interface/Hypertrie.hpp
+++ include/Dice/hypertrie/internal/node_based/interface/Hypertrie.hpp
@@ -4,6 +4,7 @@
 #include "Dice/hypertrie/internal/container/AllContainer.hpp"
 
 #include "Dice/hypertrie/internal/node_based/interface/Hypertrie_traits.hpp"
+#include "Dice/hypertrie/internal/node_based/interface/HypertrieContext.hpp"
 #include "Dice/hypertrie/internal/util/CONSTANTS.hpp"
 #include <optional>
 #include <variant>
@@ -11,9 +12,30 @@
 
 namespace hypertrie::internal::node_based {
 
-	template<typename tr = default_bool_Hypertrie_t>
+	template<typename tr>
 	class const_Hypertrie {
 	public:
+		using tri = raw::Hypertrie_internal_t<tr>;
+
+	private:
+		template<size_t depth>
+		using RawKey = typename tri::template RawKey<depth>;
+
+		template<size_t depth>
+		using RawSliceKey = typename tri::template RawSliceKey<depth>;
+	protected:
+
+		void* node_container_;
+
+		HypertrieContext<tr> *context_;
+
+		size_t depth_;
+
+		const_Hypertrie(size_t depth, HypertrieContext<tr> *context, void *raw_hypertrie) : node_container_(raw_hypertrie), context_(context), depth_(depth)  {}
+
+	public:
+
+
 		using traits = tr;
 
 		using Key = typename tr::Key;
@@ -27,7 +49,21 @@ namespace hypertrie::internal::node_based {
 		size_t size() const;
 
 		[[nodiscard]]
-		bool operator[](const Key &key) const;
+		bool operator[](const Key &key) const{
+			return compiled_switch<hypertrie_depth_limit, 1>::switch_(
+					this->depth_,
+					[&](auto depth_arg)  {
+					  RawKey<depth_arg> raw_key;
+					  std::copy_n(key.begin(), depth_arg, raw_key.begin());
+					  const auto &node_container = *static_cast<const raw::NodeContainer<depth_arg, tri>*>(this->node_container_);
+					  this->context_->raw_context.get(
+							  node_container,
+							  raw_key);
+					},
+					[]() {});
+		}
+
+
 
 		[[nodiscard]]
 		std::variant<std::optional<const_Hypertrie>, value_type> operator[](const SliceKey &slice_key) const;
@@ -52,13 +88,40 @@ namespace hypertrie::internal::node_based {
 
 	template<typename tr = default_bool_Hypertrie_t>
 	class Hypertrie : public const_Hypertrie<tr> {
+		;
 	public:
 		using traits = tr;
-
+		using tri = raw::Hypertrie_internal_t<tr>;
 		using Key = typename tr::Key;
 		using value_type = typename  tr::value_type;
 
-		void set(const Key &key, value_type value);
+	private:
+		template<size_t depth>
+		using RawKey = typename tri::template RawKey<depth>;
+
+		template<size_t depth>
+		using RawSliceKey = typename tri::template RawSliceKey<depth>;
+	public:
+
+		void set(const Key &key, value_type value) {
+			compiled_switch<hypertrie_depth_limit, 1>::switch_(
+					this->depth_,
+					[&](auto depth_arg)  {
+						RawKey<depth_arg> raw_key;
+					  	std::copy_n(key.begin(), depth_arg, raw_key.begin());
+					  	this->context_->raw_context.set(raw_key, value);
+					 },
+					[]() {});
+		}
+
+		Hypertrie(size_t depth, HypertrieContext<tr> &context = DefaultHypertrieContext<tr>::instance())
+			: const_Hypertrie<tr>(depth, &context,
+							  compiled_switch<hypertrie_depth_limit, 1>::switch_(
+									  depth,
+									  [&](auto depth_arg) -> void * {
+										  return new raw::NodeContainer<depth_arg, tri>; },
+									  [&]() -> void * { throw std::logic_error{fmt::format("Hypertrie depth {} invalid. allowed range: [1,{})", depth, hypertrie_depth_limit)}; })
+							  ) {}
 	};
 
 }

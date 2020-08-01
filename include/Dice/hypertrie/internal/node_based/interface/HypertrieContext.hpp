3f38cd663191132505659049d3ee0b6b656ab988
Alexander Bigerl <bigerl@mail.upb.de>
2020-08-01 19:55:04 +0200

    introduced DefaultHypertrieContext

--- include/Dice/hypertrie/internal/node_based/interface/HypertrieContext.hpp
+++ include/Dice/hypertrie/internal/node_based/interface/HypertrieContext.hpp
@@ -5,23 +5,48 @@
 #include "Dice/hypertrie/internal/node_based/raw/storage/NodeContext.hpp"
 #include "Dice/hypertrie/internal/util/SwitchTemplateFunctions.hpp"
 #include <fmt/format.h>
+#include <memory>
 
 
 namespace hypertrie::internal::node_based {
 
-	static constexpr const std::size_t max_depth = 5;
+	static constexpr const std::size_t hypertrie_depth_limit = 6;
 
-	template<typename tr = default_bool_Hypertrie_t>
+	template<HypertrieTrait tr = default_bool_Hypertrie_t>
 	class HypertrieContext {
-		const size_t depth_;
-		void *raw_context;
+	private:
+		static constexpr const size_t depth_ = hypertrie_depth_limit -1;
+	public:
+		using NodeContext = typename raw::template NodeContext<depth_, typename raw::template Hypertrie_internal_t<tr>>;
 
-		HypertrieContext(size_t depth) : depth_(depth) {
-			compiled_switch<max_depth, 1>::switch_(
-					depth,
-					[&](auto depth_arg) -> void * { return new raw::NodeContext<depth_arg, raw::Hypertrie_internal_t<tr>>{}; },
-					[&]() -> void * { throw std::logic_error{fmt::format("Hypertrie depth {} invalid. allowed range: [1,{})", depth, max_depth)}; });
+	public:
+		NodeContext raw_context{};
+
+	public:
+
+		HypertrieContext(){}
+
+		constexpr size_t depth() const {
+			return depth_;
 		}
+		NodeContext &rawContext() {
+			return raw_context;
+		}
+	};
+
+	template<HypertrieTrait tr>
+	class DefaultHypertrieContext {
+	public:
+		static HypertrieContext<tr>& instance(){
+			static const std::unique_ptr<HypertrieContext<tr>> instance{new HypertrieContext<tr>(hypertrie_depth_limit - 1)};
+			return *instance;
+		}
+
+		DefaultHypertrieContext(const DefaultHypertrieContext&) = delete;
+		DefaultHypertrieContext& operator= (const DefaultHypertrieContext) = delete;
+
+	protected:
+		DefaultHypertrieContext() {}
 	};
 }
 

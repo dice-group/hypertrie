cb2e5341ef249cf21add39ca7835e4426793aad7
Lukas Kerkemeier <kerkemei@campus.uni-paderborn.de>
2020-12-03 14:52:59 +0100

    made the templates used in Container_trait.h more general.

--- include/Dice/hash/Container_trait.h
+++ include/Dice/hash/Container_trait.h
@@ -10,11 +10,11 @@
 // unordered container
 template <typename T> struct is_unordered_container : std::false_type {};
 
-template <typename Key, typename Value>
-struct is_unordered_container<std::unordered_map<Key, Value>> : std::true_type {
-};
-template <typename T>
-struct is_unordered_container<std::unordered_set<T>> : std::true_type {};
+template <class Key, class T, class Hash, class KeyEqual, class Allocator>
+struct is_unordered_container<std::unordered_map<Key, T, Hash, KeyEqual, Allocator>> : std::true_type {};
+
+template <class Key, class Hash, class KeyEqual, class Allocator>
+struct is_unordered_container<std::unordered_set<Key, Hash, KeyEqual, Allocator>> : std::true_type {};
 
 template <typename T>
 constexpr bool is_unordered_container_v = is_unordered_container<T>::value;
@@ -22,11 +22,11 @@ constexpr bool is_unordered_container_v = is_unordered_container<T>::value;
 // ordered container
 template <typename T> struct is_ordered_container : std::false_type {};
 
-template <typename Key, typename Value>
-struct is_ordered_container<std::map<Key, Value>> : std::true_type {};
+template <class Key, class T, class Compare, class Allocator>
+struct is_ordered_container<std::map<Key, T, Compare, Allocator>> : std::true_type {};
 
-template <typename T>
-struct is_ordered_container<std::set<T>> : std::true_type {};
+template <class Key, class Compare, class Allocator>
+struct is_ordered_container<std::set<Key, Compare, Allocator>> : std::true_type {};
 
 template <typename T>
 constexpr bool is_ordered_container_v = is_ordered_container<T>::value;

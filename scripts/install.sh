be5cf2f02beb2a31ea239f0109da8b7c9c652026
Lennart Austenfeld <lennarta@mail.upb.de>
2021-02-02 15:32:38 +0100

    Replace apt with apt-get in scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt update
-sudo apt install -y g++-10 clang-10 make cmake python3-pip python3-setuptools python3-wheel
+sudo apt-get update
+sudo apt-get install -y g++-10 clang-10 make cmake python3-pip python3-setuptools python3-wheel
 
 pip3 install --user conan

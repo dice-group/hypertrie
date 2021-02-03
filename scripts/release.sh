b7979ed88d6303a6ec59e14ec985a0ace9ceca95
Alexander Bigerl <bigerl@mail.upb.de>
2021-02-03 17:59:24 +0100

    fixed release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
 set -e
+# change to parent dir of this script
+cd "$(dirname "$0")/.."
 
-if [ -z ${BINTRAY_API_KEY+x} ]; then
-    echo "BINTRAY_API_KEY is not set"
-    exit 1
+source scripts/internal/parse_commandline_args.sh
+source scripts/internal/get_conan_profile.sh
+
+parse_commandline_args
+get_conan_profile
+
+if [ -z "${BINTRAY_API_KEY}" ]; then
+  echo "BINTRAY_API_KEY is not set"
+  exit 1
 fi
-if [ -z ${BINTRAY_USER+x} ]; then
-    echo "BINTRAY_USER is not set"
-    exit 1
+if [ -z "${BINTRAY_USER}" ]; then
+  echo "BINTRAY_USER is not set"
+  exit 1
 fi
 
-conan user -p $BINTRAY_API_KEY -r dice-group $BINTRAY_USER
+conan user -p "$BINTRAY_API_KEY" -r dice-group "$BINTRAY_USER"
 
-if [ "$1" = "development" ] ; then
-    hypertrie_deploy_version="dev"
-elif [ "$1" = "master" ] ; then
-    hypertrie_deploy_version="latest"
+if [ "$4" = "development" ]; then
+  hypertrie_deploy_version="dev"
+elif [ "$4" = "master" ]; then
+  hypertrie_deploy_version="latest"
 else
-    hypertrie_deploy_version="$1"
+  hypertrie_deploy_version=$(conan inspect . --raw version)
 fi
-
-echo $hypertrie_deploy_version
 
 (conan remove -f "hypertrie/$hypertrie_deploy_version@dice-group/stable" || true)
-conan create . "hypertrie/$hypertrie_deploy_version@dice-group/stable" --build missing -e hypertrie_deploy_version="$hypertrie_deploy_version"
+conan create . "hypertrie/$hypertrie_deploy_version@dice-group/stable" --build missing -e hypertrie_deploy_version="$hypertrie_deploy_version" --profile ${conan_profile}
 conan upload "hypertrie/$hypertrie_deploy_version@dice-group/stable" --force --all -r dice-group
-(conan remove -f "hypertrie/$hypertrie_deploy_version@dice-group/stable" || true )
+(conan remove -f "hypertrie/$hypertrie_deploy_version@dice-group/stable" || true)

a967290521a806a3f6261df5571a3d72d334e7c5
Alexander Bigerl <bigerl@mail.upb.de>
2021-02-03 17:06:21 +0100

    extracted commandline args parsing

---
+++ scripts/internal/parse_commandline_args.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+function parse_commandline_args() {
+  # uses those variables:
+  # - build_type
+  # - hypertrie_compiler
+  # - compiler_version
+  command_layout="$(basename "$0") [build_type: {Debug, Release}] [hypertrie_compiler: {clang, gcc}] [compiler version: {10,11,..}] "
+
+  if [ "$1" == "-h" ] || [ "$1" = "--help" ]; then
+    echo "command:"
+    echo "${command_layout}"
+    exit 0
+  fi
+
+  if [ -z "${build_type}" ]; then
+    build_type=Debug
+  fi
+  if [ "${build_type}" = "Debug" ] || [ "${build_type}" = "Release" ]; then
+    echo "Do a ${build_type} build."
+  else
+    echo "release type ${build_type} is not supported"
+    echo "command:"
+    echo "${command_layout}"
+    exit 1
+  fi
+
+  if [ -z "${hypertrie_compiler}" ]; then
+    hypertrie_compiler=clang
+  fi
+  if [ "${hypertrie_compiler}" = "clang" ] || [ "${hypertrie_compiler}" = "gcc" ]; then
+    echo "Use ${hypertrie_compiler} as compiler."
+  else
+    echo "compiler ${hypertrie_compiler} is not supported"
+    echo "command:"
+    echo "${command_layout}"
+    exit 1
+  fi
+
+  if [ "${hypertrie_compiler}" = "clang" ]; then
+    if [ -z "${compiler_version}" ]; then
+      export CXX="clang++" CC="clang"
+    else
+      export CXX="clang++-${compiler_version}" CC="clang-${compiler_version}"
+    fi
+  else
+    if [ -z "${compiler_version}" ]; then
+      export CXX="g++" CC="gcc"
+    else
+      export CXX="g++-${compiler_version}" CC="gcc-${compiler_version}"
+    fi
+  fi
+
+}

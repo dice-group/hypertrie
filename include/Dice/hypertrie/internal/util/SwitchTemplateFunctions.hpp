321c3218378e43d8c86915f8c17840e453dde620
Alexander Bigerl <alexander@bigerl.eu>
2021-08-06 12:56:00 +0200

    simplified SwitchTemplateFunctions further

--- include/Dice/hypertrie/internal/util/SwitchTemplateFunctions.hpp
+++ include/Dice/hypertrie/internal/util/SwitchTemplateFunctions.hpp
@@ -6,6 +6,11 @@
 #include <cstdint>
 
 namespace hypertrie::internal {
+	/**
+     * Generates a switch case for lambdas at compile time. Evaluation of the switch-case is done at run-time.
+     * @tparam first first value of the integer range. It doesn't matter if first or last is larger. The upper bound is not excluded, i.e. [min,max)
+     * @tparam last last value of the integer range, defaults to 0
+     */
 	template<std::integral auto first, std::integral auto last = 0>
 	struct compiled_switch {
 		static constexpr bool USE_SIGNED = first < 0 or last < 0;
@@ -26,51 +31,19 @@ namespace hypertrie::internal {
 			}
 		}
 
-		template<class T, class F, integral_type i = min>
-		static void execute_void_switch(T value, F f) {
-			if constexpr (i < max) {
-				if (value == i)
-					f(std::integral_constant<T, i>{});
-				else
-					execute_switch<T, F, i + 1>(value, f);
-			} else {
-				f(std::integral_constant<T, i>{});
-			}
-		}
-
-		template<class T, class F, T... Is>
-		static auto execute_switch(T i, F f, std::integer_sequence<T, Is...>) {
-			// extract common return type
-			using return_type = std::decay_t<std::common_type_t<decltype(f(std::integral_constant<T, Is + min>{}))...>>;
-			// check if the return type is void.
-			if constexpr (std::is_same_v<return_type, void>) {
-				execute_void_switch(i, f);
-			} else {
-				return execute_switch(i, f);
-			}
-		}
-
 	public:
 		template<class F, class D>
 		static auto switch_(integral_type x, F switch_function, D default_function) {
 			if (x < min or max <= x)
 				return default_function();
 			else
-				return execute_switch(x, switch_function, std::make_index_sequence<max - min>{});
-		}
-
-		template<class F, class D>
-		static void switch_void(integral_type x, F switch_function, D default_function) {
-			if (x < min or max <= x)
-				default_function();
-			else
-				execute_void_switch(x, switch_function);
+				return execute_switch(x, switch_function);
 		}
 
 		template<class F>
-		static void switch_void(integral_type x, F switch_function) {
+		static void switch_(integral_type x, F switch_function) {
 			if (x >= min or max > x)
-				execute_void_switch(x, switch_function);
+				execute_switch(x, switch_function);
 		}
 	};
 }// namespace hypertrie::internal

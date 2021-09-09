b30df5d4ee66ff186a285b05bc70ae41af24d86a
Alexander Bigerl <alexander@bigerl.eu>
2021-09-09 11:36:30 +0200

    v0.6.0 release

--- include/Dice/hypertrie/internal/Iterator.hpp
+++ include/Dice/hypertrie/internal/Iterator.hpp
@@ -3,7 +3,6 @@
 
 #include "Dice/hypertrie/internal/raw/iterator/Iterator.hpp"
 #include "Dice/hypertrie/internal/Hypertrie_predeclare.hpp"
-#include "Dice/hypertrie/internal/old_hypertrie/BoolHypertrie.hpp"
 
 namespace hypertrie {
 
@@ -12,43 +11,36 @@ namespace hypertrie {
 		using tr = tr_t;
 		using tri = internal::raw::Hypertrie_internal_t<tr>;
 	protected:
-		template<pos_type depth>
-		using RawBoolHypertrie = typename hypertrie::internal::interface::rawboolhypertrie<typename tri::key_part_type, tri::template map_type, tri::template set_type>::template RawBoolHypertrie<depth>;
-
 		using Key = typename tr::Key;
 		using RawBaseIterator = internal::raw::base_iterator<tri>;
 		template <size_t depth>
 		using RawIterator = internal::raw::iterator<depth, tri>;
 		struct RawMethods {
 
-			void (*destruct)(const void *);
+			RawBaseIterator (*begin)(const const_Hypertrie<tr> &hypertrie) = nullptr;
 
-			void *(*begin)(const const_Hypertrie<tr> &boolHypertrie);
+			const Key &(*value)(void const *) =  nullptr;
 
-			const Key &(*value)(void const *);
+			void (*inc)(void *) =  nullptr;
 
-			void (*inc)(void *);
+			bool (*ended)(void const *) =  nullptr;
 
-			bool (*ended)(void const *);
+			RawMethods() {}
 
+			RawMethods(RawBaseIterator (*begin)(const const_Hypertrie<tr> &), const Key &(*value)(const void *), void (*inc)(void *), bool (*ended)(const void *)) : begin(begin), value(value), inc(inc), ended(ended) {}
 		};
 
 		template<pos_type depth>
 		inline static RawMethods generateRawMethods() {
-			return RawMethods{
-					[](const void *rawboolhypertrie_iterator) {
-					  using T = const typename RawBoolHypertrie<depth>::iterator;
-					  if (rawboolhypertrie_iterator != nullptr){
-						  delete static_cast<T *>(rawboolhypertrie_iterator);
-					  }
+			return RawMethods(
+					[](const const_Hypertrie<tr> &hypertrie) -> RawBaseIterator {
+					  return (RawBaseIterator) RawIterator<depth>{
+								*const_cast<internal::raw::NodeContainer<depth, tri> *>(reinterpret_cast<const internal::raw::NodeContainer<depth, tri> *>(hypertrie.rawNodeContainer())),
+							  hypertrie.context()->rawContext()};
 					},
-					[](const const_Hypertrie<tr> &boolHypertrie) -> void * {
-					  return new typename RawBoolHypertrie<depth>::iterator(
-							  *static_cast<RawBoolHypertrie<depth> const *>(boolHypertrie.rawNode()));
-					},
-					&RawBoolHypertrie<depth>::iterator::value,
-					&RawBoolHypertrie<depth>::iterator::inc,
-					&RawBoolHypertrie<depth>::iterator::ended};
+					&RawIterator<depth>::value,
+					&RawIterator<depth>::inc,
+					&RawIterator<depth>::ended);
 		}
 
 		inline static const std::vector<RawMethods> raw_method_cache = [](){
@@ -68,12 +60,10 @@ namespace hypertrie {
 			return raw_method_cache[depth - 1];
 		};
 
-		// TODO: go on here
-
 
 	protected:
 		RawMethods const * raw_methods = nullptr;
-		void *raw_iterator = nullptr;
+		RawBaseIterator raw_iterator;
 
 	public:
 		using self_type = Iterator;
@@ -85,46 +75,17 @@ namespace hypertrie {
 				raw_methods(&getRawMethods(hypertrie.depth())),
 				raw_iterator(raw_methods->begin(hypertrie)) {}
 
-		Iterator(const Iterator &) = delete;
-
-		Iterator(Iterator &&other) noexcept {
-			if (this->raw_methods != nullptr)
-				this->raw_methods->destruct(this->raw_iterator);
-			this->raw_methods = other.raw_methods;
-			this->raw_iterator = other.raw_iterator;
-			other.raw_iterator = nullptr;
-			other.raw_methods = nullptr;
-		}
-
-		Iterator &operator=(Iterator &&other) noexcept {
-			if (this->raw_methods != nullptr)
-				this->raw_methods->destruct(this->raw_iterator);
-			this->raw_methods = other.raw_methods;
-			this->raw_iterator = other.raw_iterator;
-			other.raw_iterator = nullptr;
-			other.raw_methods = nullptr;
-			return *this;
-		}
-
-		Iterator &operator=(const Iterator &) = delete;
-
-		Iterator &operator=(Iterator &) = delete;
-
-		~Iterator() {
-			if (raw_methods != nullptr)
-				raw_methods->destruct(raw_iterator);
-			raw_methods = nullptr;
-			raw_iterator = nullptr;
-		}
+		Iterator(const_Hypertrie<tr> &hypertrie) : Iterator(&hypertrie) {}
 
 		self_type &operator++() {
-			raw_methods->inc(raw_iterator);
+			raw_methods->inc(&raw_iterator);
 			return *this;
 		}
 
-		value_type operator*() const { return raw_methods->value(raw_iterator); }
+		value_type operator*() const { return raw_methods->value(&raw_iterator); }
 
-		operator bool() const { return not raw_methods->ended(raw_iterator); }
+		operator bool() const { return not raw_methods->ended(&raw_iterator); }
+
 	};
 
 }
---
+++ tests/node_based/TestSlicing.hpp
@@ -0,0 +1,160 @@
+#ifndef HYPERTRIE_TESTSLICING_HPP
+#define HYPERTRIE_TESTSLICING_HPP
+
+
+#include <iterator>
+
+#include <boost/hana/for_each.hpp>
+#include <boost/hana/range.hpp>
+
+#include <Dice/hypertrie/internal/raw/storage/NodeContext.hpp>
+
+#include <fmt/format.h>
+
+
+#include "../slice/TestSlice.hpp"
+#include "../utils/AssetGenerator.hpp"
+#include "../utils/NameOfType.hpp"
+
+
+namespace hypertrie::tests::raw::node_context::slicing {
+
+	namespace hana = boost::hana;
+	using namespace fmt::literals;
+
+
+	using namespace hypertrie::tests::utils;
+	using namespace hypertrie::internal::raw;
+
+	template<HypertrieInternalTrait tri, size_t depth, size_t max_key_part>
+	void exec_slice_test(NodeContext<5, tri> &context,
+						 NodeContainer<depth, tri> &nc,
+						 const std::map< typename tri::template RawKey<depth>, typename tri::value_type> &entries) {
+		using key_part_type [[maybe_unused]] = typename tri::key_part_type;
+		using value_type [[maybe_unused]] = typename tri::value_type;
+		using Key [[maybe_unused]] = typename tri::template RawKey<depth>;
+
+		// we need slices of each depth
+		hana::for_each(hana::range_c<size_t, 1, depth>, [&](auto fixed_depth) {
+			SECTION("slice depth: {}"_format(size_t(fixed_depth))) {
+				//			[[maybe_unused]] static constexpr size_t result_depth = depth - slice_depth;
+				using RawSliceKey = typename tri::template RawSliceKey<fixed_depth>;
+
+				RawSliceKey raw_slice_key{};
+
+				// get all the combinations for slices of depth slice_depth
+				for (auto slice_poss_tmp : iter::combinations(iter::range(depth), size_t(fixed_depth))) {
+					std::vector<size_t> fixed_poss{slice_poss_tmp.begin(), slice_poss_tmp.end()};
+					SECTION("positions: {}"_format(fmt::join(fixed_poss, ", "))) {
+						// try all possible slices keys
+						for (auto key_parts : iter::product<size_t(fixed_depth)>(iter::range(0ul, max_key_part + 1))) {
+
+							// from std::tuple to std::array
+							auto &key_parts_reint = *reinterpret_cast<std::array<size_t, size_t(fixed_depth)> *>(&key_parts);
+							SECTION("fixed key-parts: {}"_format(fmt::join(key_parts_reint, ", "))) {
+
+								CAPTURE(raw_slice_key, fixed_poss, key_parts_reint);
+
+								for (auto [slice_key_part, slice_pos, key_part] : iter::zip(raw_slice_key, fixed_poss, key_parts_reint))
+									slice_key_part = {slice_pos, key_part};
+
+								CAPTURE(raw_slice_key, fixed_poss, key_parts_reint);
+
+								TestSlice<tri, depth, fixed_depth> expected_slice{entries, raw_slice_key};
+								auto actual_slice_container = context.template slice(nc, raw_slice_key);
+
+
+								if constexpr (depth > size_t(fixed_depth)) {
+									auto &[actual_nodec, actual_is_managed] = actual_slice_container;
+
+									// check whether the slice is empty
+									REQUIRE(actual_nodec.empty() == expected_slice.empty());
+									WARN("slice empty: {}"_format(actual_nodec.empty()));
+
+									// check if the result is managed, i.e. not a slice of compressed key
+									// TODO: refactor and reactivate once implemented
+									//							REQUIRE(expected_slice.isManaged(context) == actual_is_managed);
+
+									// check size
+									REQUIRE(context.template size(actual_nodec) == expected_slice.size());
+
+									// check the entries
+									for (const auto &[expected_key, expected_value] : expected_slice.sliceEntries()) {
+										REQUIRE(context.template get(actual_nodec, expected_key) == expected_value);
+									}
+
+									if (not entries.empty()) // don't do the recursion if the parent was already empty
+										exec_slice_test<tri, depth - size_t(fixed_depth), max_key_part>(context, actual_nodec, expected_slice.sliceEntries());
+								} else {
+									auto [actual_value, actual_is_managed] = actual_slice_container.value();
+
+									// check if the result is managed, i.e. not a slice of compressed key
+									REQUIRE(expected_slice.isManaged(context) == actual_is_managed);
+
+									REQUIRE(actual_value = expected_slice.sliceEntries().begin()->second);
+								}
+							}
+						}
+					}
+				}
+			}
+		});
+	}
+
+	template<HypertrieInternalTrait tri, size_t depth>
+	void slicing_by_any_keypart() {
+		using key_part_type = typename tri::key_part_type;
+		using value_type = typename tri::value_type;
+		using Key [[maybe_unused]] = typename tri::template RawKey<depth>;
+		static constexpr const size_t max_key_part = 3;
+
+
+		static utils::RawGenerator<depth, key_part_type, value_type, size_t(tri::is_lsb_unused)> gen{0, max_key_part};
+		gen.setValueMinMax(0, value_type(max_key_part*2));
+
+		for (auto number_of_entries : std::vector{0ul, 1ul, size_t(std::pow(max_key_part, depth) / 2), size_t(std::pow(max_key_part, depth) - depth)})
+			SECTION("entries in hypertrie: {}"_format(number_of_entries))
+			for (auto i : iter::range(50))
+				SECTION("hypertrie {}"_format(i)) {
+
+					auto entries = gen.entries(number_of_entries);
+
+					NodeContext<5, tri> context;
+
+					// create emtpy primary node
+					NodeContainer<depth, tri> nc{};
+
+
+					std::string print_entries{};
+					for (auto &[key, value] : entries)
+						print_entries += "{} → {}\n"_format(key, value);
+					WARN(print_entries);
+
+					// bulk insert keys
+					for (const auto &[key, value] : entries)
+						context.template set<depth>(nc, key, value);
+
+					exec_slice_test<tri, depth, max_key_part>(context, nc, entries);
+				}
+
+	}
+
+	TEMPLATE_TEST_CASE_SIG("test slicing [bool]", "[Slicing]", ((size_t depth), depth), 2, 3) {
+		slicing_by_any_keypart<default_bool_Hypertrie_internal_t, depth>();
+	}
+
+	TEMPLATE_TEST_CASE_SIG("test slicing [bool lsb-unused]", "[Slicing]", ((size_t depth), depth), 2, 3) {
+		slicing_by_any_keypart<lsbunused_bool_Hypertrie_internal_t , depth>();
+	}
+
+	TEMPLATE_TEST_CASE_SIG("test slicing [long]", "[Slicing]", ((size_t depth), depth), 2, 3) {
+		slicing_by_any_keypart<default_long_Hypertrie_internal_t , depth>();
+	}
+
+	TEMPLATE_TEST_CASE_SIG("test slicing [double]", "[Slicing]", ((size_t depth), depth), 2, 3) {
+		slicing_by_any_keypart<default_double_Hypertrie_internal_t , depth>();
+	}
+
+}// namespace hypertrie::tests::raw::node_context::slicing
+
+#endif//HYPERTRIE_TESTSLICING_HPP

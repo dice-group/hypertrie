94f47569e935538f84b52b1554da6d471a80ec7d
Lukas Kerkemeier <lukas.kerkemeier@web.de>
2021-10-18 15:30:39 +0200

    Moved the metall test to its own file, updated from v0.15 to v0.17 (metall).

--- tests/core/node/tests_RawHypertrieContext_systematic_metall.cpp
+++ tests/core/node/tests_RawHypertrieContext_systematic_metall.cpp
@@ -21,8 +21,22 @@
 
 #include <EntrySetGenerator.hpp>
 
+#include <metall/metall.hpp>
+//where do we include this??
+#include <tsl/boost_offset_pointer.h>
+
 namespace hypertrie::tests::core::node {
-	TEST_SUITE("systematic testing of RawNodeContext") {
+
+	using metall_bool_Hypertrie_trait = Hypertrie_trait<unsigned long,
+													 bool,
+													 metall::manager::allocator_type<std::byte>,
+													 hypertrie::internal::container::tsl_sparse_map,
+													 hypertrie::internal::container::tsl_sparse_set>;
+	template<size_t depth>
+	using metall_bool_cfg = Node_test_config<depth, metall_bool_Hypertrie_trait>;
+
+
+	TEST_SUITE("systematic testing of RawNodeContext with the metall allocator") {
 		using namespace ::hypertrie::internal::raw;
 		using namespace ::hypertrie::internal::util;
 
@@ -30,46 +44,60 @@ namespace hypertrie::tests::core::node {
 				 size_t no_key_parts,
 				 size_t min_no_entries,
 				 size_t max_no_entries>
-		void write_and_read() {
-			using SingleEntry_t = SingleEntry<depth, tri_with_stl_alloc<tri>>;
-
+		void metall_write_and_read() {
+			using stl_alloc_trait = tri_with_stl_alloc<tri>;
+			using SingleEntry_t = SingleEntry<depth, stl_alloc_trait>;
 			using key_part_type = typename tri::key_part_type;
-
 			static constexpr auto max_key_part = key_part_type(no_key_parts);
+			const std::string path = "/tmp";
+			const std::string contextName = "someNameForTheObjectCreatedByMetall1";
+			const std::string containerName = "someNameForTheObjectCreatedByMetall2";
 
+			//create segment
+			{
+				metall::manager manager(metall::create_only, path.c_str());
+			}
+			//create allocator and manager to use
+			metall::manager manager(metall::open_only, path.c_str());
+			auto alloc = manager.get_allocator();
+
+			auto generateConAndNC = [&manager, &alloc, &contextName, &containerName]() {
+				auto &context = *manager.construct<RawHypertrieContext<depth, tri>>(contextName.c_str())(alloc);
+				auto &nc = *manager.construct<NodeContainer<depth, tri>>(containerName.c_str())();
+				return std::make_tuple(context, nc);
+			};
+			auto deleteConAndNC = [&manager, &contextName, &containerName]() {
+				manager.destroy<RawHypertrieContext<depth, tri>>(contextName.c_str());
+				manager.destroy<NodeContainer<depth, tri>>(containerName.c_str());
+			};
 
 			SUBCASE("{}"_format(tri{}).c_str()) {
 				SUBCASE("hypertrie depth = {}"_format(depth).c_str()) {
 					boost::hana::for_each(boost::hana::range_c<size_t, min_no_entries, max_no_entries + 1>, [&](auto no_entries_0) {
 						boost::hana::for_each(boost::hana::range_c<size_t, min_no_entries, max_no_entries + 1>, [&](auto no_entries_1) {
 							SUBCASE("first {} entries, then {} entries"_format(no_entries_0, no_entries_1).c_str()) {
-
-
-								utils::EntrySetGenerator<depth, no_entries_0, tri, max_key_part> outer_generator{};
+								utils::EntrySetGenerator<depth, no_entries_0, stl_alloc_trait, max_key_part> outer_generator{};
 								for (const auto &entries_0 : outer_generator) {
 									SUBCASE("first_entries: {}"_format(fmt::join(entries_0, " | ")).c_str()) {
-										RawHypertrieContext<depth, tri> context{std::allocator<std::byte>()};
-										NodeContainer<depth, tri> nc{};
-
+										auto [context, nc] = generateConAndNC();
 										context.insert(nc, entries_0);
-										ValidationRawNodeContext<depth, tri> validation_context_0{std::allocator<std::byte>(), entries_0};
+										ValidationRawNodeContext<depth, stl_alloc_trait> validation_context_0{std::allocator<std::byte>(), entries_0};
 										CHECK(context == validation_context_0);
 										std::cout << fmt::format("result identifier: {}", nc.raw_identifier()) << std::endl;
 
-										utils::EntrySetGenerator_with_exclude<depth, no_entries_1, tri, max_key_part> inner_generator{entries_0};
+										utils::EntrySetGenerator_with_exclude<depth, no_entries_1, stl_alloc_trait, max_key_part> inner_generator{entries_0};
 										for (const auto &entries_1 : inner_generator) {
 											SUBCASE("second_entries: {}"_format(fmt::join(entries_1, " | ")).c_str()) {
 												std::vector<SingleEntry_t> all_entries = entries_0;
 												all_entries.insert(all_entries.end(), entries_1.begin(), entries_1.end());
-
 												context.insert(nc, entries_1);
 												std::cout << fmt::format("result identifier: {}", nc.raw_identifier()) << std::endl;
-												// std::cout << fmt::format("Actual:\n {}", context) << std::endl;
-												ValidationRawNodeContext<depth, tri> validation_context{std::allocator<std::byte>(), all_entries};
-												// std::cout << fmt::format("Verification:\n {}", (RawHypertrieContext<depth, tri>&)validation_context) << std::endl;
+												ValidationRawNodeContext<depth, stl_alloc_trait> validation_context{std::allocator<std::byte>(), all_entries};
 												CHECK(context == validation_context);
 											}
 										}
+
+										deleteConAndNC();
 									}
 								}
 							}
@@ -78,7 +106,6 @@ namespace hypertrie::tests::core::node {
 				}
 			}
 		}
-
 
 		TEST_CASE("entry_generator") {
 			using config = bool_cfg<2>;
@@ -95,64 +122,11 @@ namespace hypertrie::tests::core::node {
 		}
 
 		TEST_CASE_TEMPLATE("hypertrie depth 1", T,
-						   bool_cfg<1>,
-						   tagged_bool_cfg<1>,
-						   long_cfg<1>,
-						   double_cfg<1>) {
+						   metall_bool_cfg<1> /*, metall_bool_cfg<2>*/) {
 			constexpr size_t no_key_parts = 3;
 			constexpr size_t min_no_entries = 1;
 			constexpr size_t max_no_entries = 3;
-			write_and_read<T::depth, typename T::tri, no_key_parts, min_no_entries, max_no_entries>();
-		}
-
-		TEST_CASE_TEMPLATE("hypertrie depth 2", T,
-						   bool_cfg<2>,
-						   tagged_bool_cfg<2>,
-						   long_cfg<2>,
-						   double_cfg<2>) {
-			constexpr size_t no_key_parts = 2;
-			constexpr size_t min_no_entries = 1;
-			constexpr size_t max_no_entries = 2;
-			write_and_read<T::depth, typename T::tri, no_key_parts, min_no_entries, max_no_entries>();
-		}
-
-		TEST_CASE_TEMPLATE("hypertrie depth 3", T,
-						   bool_cfg<3>,
-						   tagged_bool_cfg<3>,
-						   long_cfg<3>,
-						   double_cfg<3>) {
-			{
-				constexpr size_t no_key_parts = 3;
-				constexpr size_t min_no_entries = 1;
-				constexpr size_t max_no_entries = 1;
-				write_and_read<T::depth, typename T::tri, no_key_parts, min_no_entries, max_no_entries>();
-			}
-			{
-				constexpr size_t no_key_parts = 2;
-				constexpr size_t min_no_entries = 1;
-				constexpr size_t max_no_entries = 3;
-				write_and_read<T::depth, typename T::tri, no_key_parts, min_no_entries, max_no_entries>();
-			}
-
-		}
-
-		TEST_CASE_TEMPLATE("hypertrie depth 4", T,
-						   bool_cfg<4>,
-						   tagged_bool_cfg<4>,
-						   long_cfg<4>,
-						   double_cfg<4>) {
-			{
-				constexpr size_t no_key_parts = 3;
-				constexpr size_t min_no_entries = 1;
-				constexpr size_t max_no_entries = 1;
-				write_and_read<T::depth, typename T::tri, no_key_parts, min_no_entries, max_no_entries>();
-			}
-			{
-				constexpr size_t no_key_parts = 2;
-				constexpr size_t min_no_entries = 2;
-				constexpr size_t max_no_entries = 3;
-				write_and_read<T::depth, typename T::tri, no_key_parts, min_no_entries, max_no_entries>();
-			}
+			metall_write_and_read<T::depth, typename T::tri, no_key_parts, min_no_entries, max_no_entries>();
 		}
 	}
-}// namespace hypertrie::tests::core::node
+}

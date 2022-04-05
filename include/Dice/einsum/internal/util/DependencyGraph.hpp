16d4dea5ea14ce5452e962c5ddd18dedec157a1a
nkaralis <nkaralis@mail.uni-paderborn.de>
2022-04-05 10:49:26 +0200

    minor fix related to boost graph

--- include/Dice/einsum/internal/util/DependencyGraph.hpp
+++ include/Dice/einsum/internal/util/DependencyGraph.hpp
@@ -7,13 +7,13 @@
 #include <variant>
 
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graph_traits.hpp>
 #include <boost/graph/connected_components.hpp>
+#include <boost/graph/graph_traits.hpp>
 #include <boost/graph/strong_components.hpp>
 
-#include <tsl/hopscotch_set.h>
-#include <tsl/hopscotch_map.h>
 #include <itertools.hpp>
+#include <tsl/hopscotch_map.h>
+#include <tsl/hopscotch_set.h>
 
 namespace einsum::internal::util {
 
@@ -22,11 +22,10 @@ namespace einsum::internal::util {
 	 * @tparam VertexType the type of the vertices of the graph
 	 * @tparam EdgeLabel the type of the labels of the edges of the graph
 	 */
-    template<typename VertexType = uint32_t, typename EdgeLabel = char>
-    class DependencyGraph {
+	template<typename VertexType = uint32_t, typename EdgeLabel = char>
+	class DependencyGraph {
 
-    private:
-
+	private:
 		struct LabelledEdge {
 			EdgeLabel label;
 			bool wwd;
@@ -38,17 +37,23 @@ namespace einsum::internal::util {
 															boost::no_property,
 															LabelledEdge>;
 
-        using DirectedGraph = boost::adjacency_list<boost::vecS,
+		using LabelledGraph = boost::adjacency_list<boost::vecS,
+													boost::vecS,
+													boost::undirectedS,
+													boost::no_property,
+													LabelledEdge>;
+
+		using DirectedGraph = boost::adjacency_list<boost::vecS,
 													boost::vecS,
 													boost::directedS,
 													boost::no_property,
 													boost::no_property>;
 
 		using VertexDesc = typename boost::graph_traits<DirectedLabelledGraph>::vertex_descriptor;
-		using StrongComponentID = VertexType ;
-		using WeakComponentID = VertexType ;
+		using StrongComponentID = VertexType;
+		using WeakComponentID = VertexType;
 		static_assert(std::is_same_v<VertexDesc, std::size_t>);
-        using WeakComponentLabels_t = tsl::hopscotch_set<EdgeLabel>;
+		using WeakComponentLabels_t = tsl::hopscotch_set<EdgeLabel>;
 
 		// it used to capture dependencies between operands in joins and left-joins
 		DirectedLabelledGraph graph{};
@@ -56,8 +61,7 @@ namespace einsum::internal::util {
 		// it used to capture dependencies between sub_operators in cartesian joins
 		DirectedGraph unlabelled_graph{};
 
-    public:
-
+	public:
 		bool wwd = false;
 
 		struct StrongComponent {
@@ -69,12 +73,10 @@ namespace einsum::internal::util {
 		};
 
 	private:
-
 		using IndependentStrongComponents = std::vector<StrongComponent>;
-        IndependentStrongComponents ind_strong_comp{};
+		IndependentStrongComponents ind_strong_comp{};
 
 	public:
-
 		DependencyGraph() = default;
 
 		void addVertex() {
@@ -83,8 +85,8 @@ namespace einsum::internal::util {
 		}
 
 		void addEdge(EdgeLabel label, VertexDesc source, VertexDesc target, bool wwd = false) {
-			if(!this->wwd and wwd)
-			    this->wwd = wwd;
+			if (!this->wwd and wwd)
+				this->wwd = wwd;
 			boost::add_edge(source, target, LabelledEdge{label, wwd}, graph);
 		}
 
@@ -95,89 +97,92 @@ namespace einsum::internal::util {
 		std::vector<Label> getIncomingLabels(VertexDesc target) {
 			std::vector<Label> incoming_labels{};
 			auto vertices_iterators = boost::vertices(graph);
-            for(auto vertex_iter = vertices_iterators.first; vertex_iter != vertices_iterators.second; vertex_iter++) {
-				if(*vertex_iter == target)
+			for (auto vertex_iter = vertices_iterators.first; vertex_iter != vertices_iterators.second; vertex_iter++) {
+				if (*vertex_iter == target)
 					continue;
-                auto out_edges_iterators = boost::out_edges(*vertex_iter, graph);
-                for(auto out_edge_iter = out_edges_iterators.first; out_edge_iter != out_edges_iterators.second; out_edge_iter++) {
-					if(target == boost::target(*out_edge_iter, graph))
+				auto out_edges_iterators = boost::out_edges(*vertex_iter, graph);
+				for (auto out_edge_iter = out_edges_iterators.first; out_edge_iter != out_edges_iterators.second; out_edge_iter++) {
+					if (target == boost::target(*out_edge_iter, graph))
 						incoming_labels.emplace_back(graph[*out_edge_iter].label);
 				}
 			}
 			return incoming_labels;
 		}
 
-        tsl::hopscotch_set<VertexType> getTransitiveNeighbors(VertexDesc vertex) {
-            tsl::hopscotch_set<VertexType> target_vertices{};
+		tsl::hopscotch_set<VertexType> getTransitiveNeighbors(VertexDesc vertex) {
+			tsl::hopscotch_set<VertexType> target_vertices{};
 			std::deque<VertexDesc> to_check{vertex};
 			tsl::hopscotch_set<VertexDesc> visited{};
 			bool flag = true;
-			while(!to_check.empty()) {
+			while (!to_check.empty()) {
 				auto v = to_check.front();
 				to_check.pop_front();
-				if(visited.find(v) != visited.end())
+				if (visited.find(v) != visited.end())
 					continue;
-			    visited.insert(v);
-                auto out_edges_iterators = boost::out_edges(v, graph);
-                for(auto out_edge_iter = out_edges_iterators.first; out_edge_iter != out_edges_iterators.second; out_edge_iter++) {
+				visited.insert(v);
+				auto out_edges_iterators = boost::out_edges(v, graph);
+				for (auto out_edge_iter = out_edges_iterators.first; out_edge_iter != out_edges_iterators.second; out_edge_iter++) {
 					// skip the edge if it is used to capture wwd dependencies and is an immediate neighbor
-					if(graph[*out_edge_iter].wwd and flag)
+					if (graph[*out_edge_iter].wwd and flag)
 						continue;
-                    auto target = boost::target(*out_edge_iter, graph);
-                    if(target == v)
-                        continue;
-                    target_vertices.insert(boost::target(*out_edge_iter, graph));
-                    to_check.push_back(boost::target(*out_edge_iter, graph));
-                }
+					auto target = boost::target(*out_edge_iter, graph);
+					if (target == v)
+						continue;
+					target_vertices.insert(boost::target(*out_edge_iter, graph));
+					to_check.push_back(boost::target(*out_edge_iter, graph));
+				}
 				flag = false;
-            }
-            return target_vertices;
-        }
+			}
+			return target_vertices;
+		}
 
-        [[nodiscard]] tsl::hopscotch_set<VertexType> getTransitiveNeighborsUnlabelled(VertexDesc vertex) {
-            tsl::hopscotch_set<VertexType> target_vertices{};
-            std::deque<VertexDesc> to_check{vertex};
-            tsl::hopscotch_set<VertexDesc> visited{};
-            while(!to_check.empty()) {
-                auto v = to_check.front();
-                to_check.pop_front();
-                if(visited.find(v) != visited.end())
-                    continue;
-                visited.insert(v);
-                auto out_edges_iterators = boost::out_edges(v, unlabelled_graph);
-                for(auto out_edge_iter = out_edges_iterators.first; out_edge_iter != out_edges_iterators.second; out_edge_iter++) {
-                    auto target = boost::target(*out_edge_iter, unlabelled_graph);
-                    if(target == v)
-                        continue;
-                    target_vertices.insert(boost::target(*out_edge_iter, unlabelled_graph));
-                    to_check.push_back(boost::target(*out_edge_iter, unlabelled_graph));
-                }
-            }
-            return target_vertices;
-        }
+		[[nodiscard]] tsl::hopscotch_set<VertexType> getTransitiveNeighborsUnlabelled(VertexDesc vertex) {
+			tsl::hopscotch_set<VertexType> target_vertices{};
+			std::deque<VertexDesc> to_check{vertex};
+			tsl::hopscotch_set<VertexDesc> visited{};
+			while (!to_check.empty()) {
+				auto v = to_check.front();
+				to_check.pop_front();
+				if (visited.find(v) != visited.end())
+					continue;
+				visited.insert(v);
+				auto out_edges_iterators = boost::out_edges(v, unlabelled_graph);
+				for (auto out_edge_iter = out_edges_iterators.first; out_edge_iter != out_edges_iterators.second; out_edge_iter++) {
+					auto target = boost::target(*out_edge_iter, unlabelled_graph);
+					if (target == v)
+						continue;
+					target_vertices.insert(boost::target(*out_edge_iter, unlabelled_graph));
+					to_check.push_back(boost::target(*out_edge_iter, unlabelled_graph));
+				}
+			}
+			return target_vertices;
+		}
 
 		// treats the directed graph as an undirected graph
 		// finds the connected components of the undirected graph
 		// returns the labels of each component
-        [[nodiscard]] std::vector<WeakComponentLabels_t> getWeaklyConnectedComponentsLabels() {
+		[[nodiscard]] std::vector<WeakComponentLabels_t> getWeaklyConnectedComponentsLabels() {
+			// create an undirected copy of the graph
+			LabelledGraph undirected_graph;
+			boost::copy_graph(graph, undirected_graph);
 			// stores by position (Vertex) which component it belongs to (entry value)
-            std::vector<WeakComponentID> component(boost::num_vertices(graph));
-            auto num_components = boost::connected_components(graph, &component[0]);
+			std::vector<WeakComponentID> component(boost::num_vertices(undirected_graph));
+			auto num_components = boost::connected_components(undirected_graph, &component[0]);
 
-            std::vector<WeakComponentLabels_t> weakly_connected_components(num_components);
+			std::vector<WeakComponentLabels_t> weakly_connected_components(num_components);
 
-            for(std::size_t i : iter::range(component.size())) {
-                auto out_edges_iterator = boost::out_edges(i, graph);
-                for(auto out_edge_it = out_edges_iterator.first; out_edge_it != out_edges_iterator.second; out_edge_it++) {
+			for (std::size_t i : iter::range(component.size())) {
+				auto out_edges_iterator = boost::out_edges(i, graph);
+				for (auto out_edge_it = out_edges_iterator.first; out_edge_it != out_edges_iterator.second; out_edge_it++) {
 					weakly_connected_components[component[i]].insert(graph[*out_edge_it].label);
 				}
 			}
-            return weakly_connected_components;
-        }
+			return weakly_connected_components;
+		}
 
-        // https://www.boost.org/doc/libs/1_74_0/libs/graph/example/strong_components.cpp
-        [[nodiscard]] IndependentStrongComponents getIndependentStrongComponent() {
-			if(not ind_strong_comp.empty())
+		// https://www.boost.org/doc/libs/1_74_0/libs/graph/example/strong_components.cpp
+		[[nodiscard]] IndependentStrongComponents getIndependentStrongComponent() {
+			if (not ind_strong_comp.empty())
 				return ind_strong_comp;
 
 			std::vector<StrongComponentID> component(num_vertices(graph)),
@@ -185,33 +190,33 @@ namespace einsum::internal::util {
 			std::vector<boost::default_color_type> color(num_vertices(graph));
 			std::vector<VertexDesc> root(num_vertices(graph));
 			auto num_components = boost::strong_components(graph,
-												   make_iterator_property_map(component.begin(), get(boost::vertex_index, graph)),
-												   root_map(make_iterator_property_map(root.begin(), get(boost::vertex_index, graph)))
-														   .color_map(
-																   make_iterator_property_map(color.begin(), get(boost::vertex_index, graph)))
-														   .discover_time_map(make_iterator_property_map(
-																   discover_time.begin(), get(boost::vertex_index, graph))));
+														   make_iterator_property_map(component.begin(), get(boost::vertex_index, graph)),
+														   root_map(make_iterator_property_map(root.begin(), get(boost::vertex_index, graph)))
+																   .color_map(
+																		   make_iterator_property_map(color.begin(), get(boost::vertex_index, graph)))
+																   .discover_time_map(make_iterator_property_map(
+																		   discover_time.begin(), get(boost::vertex_index, graph))));
 
 			std::vector<StrongComponent> strongly_connected_components(num_components);
 
-            for(std::size_t i : iter::range(component.size())) {
+			for (std::size_t i : iter::range(component.size())) {
 				auto cur_component_idx = component[i];
 				auto out_edges_iterator = boost::out_edges(i, graph);
 				strongly_connected_components[cur_component_idx].vertices_out_edges_labels[i];
 				for (auto out_edge = out_edges_iterator.first; out_edge != out_edges_iterator.second; out_edge++) {
 					auto out_vertex = boost::target(*out_edge, graph);
-					if(i == out_vertex)
+					if (i == out_vertex)
 						continue;
 					auto out_edge_label = graph[*out_edge].label;
-                    strongly_connected_components[cur_component_idx].vertices_out_edges_labels[i].insert(out_edge_label);
+					strongly_connected_components[cur_component_idx].vertices_out_edges_labels[i].insert(out_edge_label);
 					auto out_vertex_component_idx = component[out_vertex];
 					if (cur_component_idx == out_vertex_component_idx) {
 						strongly_connected_components[cur_component_idx].component_labels.insert(out_edge_label);
 					} else {
 						strongly_connected_components[cur_component_idx].outgoing_labels.insert(out_edge_label);
 						strongly_connected_components[out_vertex_component_idx].incoming_labels.insert(out_edge_label);
-						if(graph[*out_edge].wwd)
-                            strongly_connected_components[cur_component_idx].wwd_labels.insert(out_edge_label);
+						if (graph[*out_edge].wwd)
+							strongly_connected_components[cur_component_idx].wwd_labels.insert(out_edge_label);
 					}
 				}
 			}
@@ -220,12 +225,11 @@ namespace einsum::internal::util {
 				if (scc.incoming_labels.empty()) {
 					ind_strong_comp.push_back(scc);
 				}
-		    }
+			}
 			return ind_strong_comp;
 		}
+	};
 
-    };
+}// namespace einsum::internal::util
 
-}
-
-#endif //SPARSETENSOR_EINSUM_UTIL_DIRECTEDGRAPH_HPP
+#endif//SPARSETENSOR_EINSUM_UTIL_DIRECTEDGRAPH_HPP
